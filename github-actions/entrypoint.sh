0edcf9dbe1244f6697e8a40d16ea6473b3967c29
youyo <1003ni2@gmail.com>
2019-11-10 00:59:03 +0900

    fix

--- github-actions/entrypoint.sh
+++ github-actions/entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -ue
 
-echo ${INPUT_ROLE_ARN} > aaa
-cat aaa
-
 eval "$(/awscredswrap --role-arn ${INPUT_ROLE_ARN} --role-session-name ${INPUT_ROLE_SESSION_NAME} --duration-seconds ${INPUT_DURATION_SECONDS})"
 
 echo ::add-mask::${AWS_ACCESS_KEY_ID}
